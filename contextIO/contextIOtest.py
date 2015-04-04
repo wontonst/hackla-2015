6a53910a05dc392cd316a45f7f3389e0d231b2c0
Akhilesh Saklecha <saklecha@usc.edu>
2015-04-04 08:19:12 -0700

    message body extraction from email

--- contextIO/contextIOtest.py
+++ contextIO/contextIOtest.py
@@ -8,7 +8,18 @@ context_io = c.ContextIO(
     consumer_secret=CONSUMER_SECRET
 )
 
-params = { 'id': '551fa8ee8e079d711e8b4568'}
-accounts = c.Account(context_io,params)
-print(accounts)
+# params = { 'id': '551fa8ee8e079d711e8b4568'}
+# accounts = c.Account(context_io,params)
+accounts = context_io.get_accounts(email='tempforhacks@gmail.com')
+
+for account in accounts:
+	sync_result = account.post_sync()
+	if(sync_result['success']):
+		for message in account.get_messages():
+			try :
+				message_body = message.get_body()
+				if(message_body[0]['type'] == 'text/plain'):
+					print(message_body[0]['content'])
+			except:
+				pass
 
