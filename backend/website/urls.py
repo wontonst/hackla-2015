b2b87d1246601723ade10f22dc64f670ab4fe73b
iforapsy <yamijack2000@gmail.com>
2015-04-04 07:45:09 -0700

    Add user signup feature

--- backend/website/urls.py
+++ backend/website/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^index.html$', views.index, name='index'),
     url(r'^$', views.index, name='index'),
+    url(r'^signup.html$', views.signup, name='signup'),
+    # url(r'^createuser.html$', views.createuser, name='createuser'),
 ]
--- backend/website/views.py
+++ backend/website/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
     template = loader.get_template('website/index.html')
     return HttpResponse(template.render())
+
+def signup(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['passwd']
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+
+        user = User.objects.create_user(email, email, password, first_name = firstname, last_name = lastname) 
+
+        return render(request, 'website/usercreated.html', 
+            {'email': email, 
+            'firstname': firstname, 
+            'lastname': lastname })
+    else:
+        return render(request, 'website/signup.html', {})
+
