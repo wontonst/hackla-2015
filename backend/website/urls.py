4eb4f12312e08c00a18fee4e202329ed0d8a02f8
iforapsy <yamijack2000@gmail.com>
2015-04-05 12:18:19 -0700

    Make contextIOtest.p integratable.

--- backend/website/urls.py
+++ backend/website/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^index.html$', views.index, name='index'),
     url(r'^$', views.index, name='index'),
+    url(r'^index.html$', views.index, name='index'),
     url(r'^signup.html$', views.signup, name='signup'),
     url(r'^signin.html$', views.signin, name='signin'),
     url(r'^mirror.html$', views.mirror, name='mirror'),
--- contextIO/contextIOtest.py
+++ contextIO/contextIOtest.py
@@ -6,31 +6,40 @@ from collections import Counter
 CONSUMER_KEY = '9rr04zqo'
 CONSUMER_SECRET = '1nUuKJkXFC03dbHD'
 
-context_io = c.ContextIO(
-    consumer_key=CONSUMER_KEY, 
-    consumer_secret=CONSUMER_SECRET
-)
+def main():
+    context_io = c.ContextIO(
+        consumer_key=CONSUMER_KEY, 
+        consumer_secret=CONSUMER_SECRET
+    )
+    
+    # params = { 'id': '551fa8ee8e079d711e8b4568'}
+    # accounts = c.Account(context_io,params)
+    accounts = context_io.get_accounts(email='tempforhacks@gmail.com')
 
-# params = { 'id': '551fa8ee8e079d711e8b4568'}
-# accounts = c.Account(context_io,params)
-accounts = context_io.get_accounts(email='tempforhacks@gmail.com')
+    list = []
+    
+    for account in accounts:
+        sync_result = account.post_sync()
+        if(sync_result['success']):
+            for message in account.get_messages():
+                try :
+                    message_body = message.get_body()
+    				#print(message_body[0]['type'])
+                    if(message_body[0]['type'] == 'text/plain'):
+    					#print(len(message_body[0]['content']))
+                        request_data = Counter()
+                        email_body = message_body[0]['content']
+                        request_data['txt'] = email_body
+                        r = requests.post('http://sentiment.vivekn.com/api/text/',data=request_data)
+    					#print('1')
+                        if(r):
+                            sentiment = r.json()['result']['sentiment']
+                            list.append((message_body[0]['content'], sentiment))
+    					    #this is where the email message body is being extracted, we can use this to get the emails
+                except:
+                    pass
 
-for account in accounts:
-	sync_result = account.post_sync()
-	if(sync_result['success']):
-		for message in account.get_messages():
-			try :
-				message_body = message.get_body()
-				#print(message_body[0]['type'])
-				if(message_body[0]['type'] == 'text/plain'):
-					#print(len(message_body[0]['content']))
-					request_data = Counter()
-					request_data['txt'] = message_body[0]['content']
-					r = requests.post('http://sentiment.vivekn.com/api/text/',data=request_data)
-					#print('1')
-					if(r):
-						print(r.json()['result']['sentiment'])
-					#this is where the email message body is being extracted, we can use this to get the emails
-			except:
-				pass
-
+    return list
+    
+if __name__ == '__main__':
+    list = main()
