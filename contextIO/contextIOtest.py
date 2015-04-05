3a8f4e3d91553c58d9dd1bf65804ee807442afe1
Akhilesh Saklecha <saklecha@usc.edu>
2015-04-04 23:50:18 -0700

    sentiment analysis

--- contextIO/contextIOtest.py
+++ contextIO/contextIOtest.py
@@ -1,4 +1,7 @@
 import contextio as c
+import requests
+import json
+from collections import Counter
 
 CONSUMER_KEY = '9rr04zqo'
 CONSUMER_SECRET = '1nUuKJkXFC03dbHD'
@@ -19,7 +22,12 @@ for account in accounts:
 			try :
 				message_body = message.get_body()
 				if(message_body[0]['type'] == 'text/plain'):
-					print(message_body[0]['content'])
+					#print(message_body[0]['content'])
+					request_data = Counter()
+					request_data['txt'] = message_body[0]['content']
+					r = requests.post('http://sentiment.vivekn.com/api/text/',data=request_data)
+					if(r):
+						print(r.json()['result']['sentiment'])
 					#this is where the email message body is being extracted, we can use this to get the emails
 			except:
 				pass
